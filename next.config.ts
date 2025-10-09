9a4f393e87a4c5b661d20ffc54fdf434dcb69743
George Gabriel <gandrewgabriel.academic@gmail.com>
2025-10-09 14:00:05 +0100

    Replace SVGR webpack config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  webpack(config) {
+    webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule: any) =>
       rule.test?.test?.('.svg'),
@@ -29,7 +29,8 @@ const nextConfig: NextConfig = {
 
     return config
   },
-}
+};
+
 
 
 export default nextConfig;
