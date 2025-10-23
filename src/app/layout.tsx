6efaf32e3f7d24fb1021aa6065d68ab81cd848e2
George Gabriel <gandrewgabriel.academic@gmail.com>
2025-10-23 10:06:54 +0100

    Use useEffect to set participant timestamp ID, and change header metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,12 @@
+"use client"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/context/ThemeContext";
 import { PageProvider } from "@/context/PageContext";
+import { useEffect, useState } from "react";
+import Head from "next/head";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Cognitive Abilities Study",
-  description: "Tasks for the cognitive abilities study",
-};
 
 export default function RootLayout({
   children,
@@ -25,37 +14,40 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
+  const [startTimestamp, setStartTimestamp] = useState("")
 
-  function generateTimestamp(): string {
+  useEffect(() => {
     const now = new Date();
-
     const formattedTimestamp = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}_${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}${String(now.getSeconds()).padStart(2, '0')}`;
-
-    return formattedTimestamp;
-}
+    setStartTimestamp(formattedTimestamp)
+  }, []);
 
   return (
     <ThemeProvider>
-      <PageProvider participantID={`p_${generateTimestamp()}`} pages={
-        ["introduction", 
-        "demographic-survey", 
-        "go-nogo", 
-        "score", 
-        "matrix-reasoning", 
-        "score", 
-        "memory-span", 
-        "score", 
-        "visual-search", 
-        "score", 
-        "string-transformation", 
-        "score" ]} startPageIndex={0}>
-      <html lang="en" className="dark">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-slate-900 dark:text-white`}
-        >
-          {children}
-        </body>
-      </html>
+      <PageProvider participantID={`p_${startTimestamp}`} pages={
+        ["introduction",
+          "demographic-survey",
+          "go-nogo",
+          "score",
+          "matrix-reasoning",
+          "score",
+          "memory-span",
+          "score",
+          "visual-search",
+          "score",
+          "string-transformation",
+          "score"]} startPageIndex={0}>
+        <html lang="en" className="dark">
+          <Head>
+            <title>Cognitive Abilities Study</title>
+            <meta name="description" content="Tasks for the cognitive abilities study" />
+          </Head>
+          <body
+            className={`antialiased dark:bg-slate-900 dark:text-white`}
+          >
+            {children}
+          </body>
+        </html>
       </PageProvider>
     </ThemeProvider>
   );
