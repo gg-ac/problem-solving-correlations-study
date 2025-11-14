39eb6fcc35e084d48f6f722eb6085fb4b870e0eb
George Gabriel <gandrewgabriel.academic@gmail.com>
2025-11-14 11:37:47 +0000

    Adds sound test and makes error sound quieter

--- src/app/tasks/introduction/page.tsx
+++ src/app/tasks/introduction/page.tsx
@@ -2,6 +2,7 @@
 
 import { usePageContext } from "@/context/PageContext";
 import { useEffect } from "react";
+import { Howl } from 'howler';
 
 export default function Home() {
     const requestFullscreen = (): void => {
@@ -15,6 +16,38 @@ export default function Home() {
         requestFullscreen();
     }, []);
 
+
+
+    const soundFiles = [
+        '/problem-solving-correlations-study/audio/digits/sound_0.mp3',
+        '/problem-solving-correlations-study/audio/sound_effects/error_sound.mp3',
+        '/problem-solving-correlations-study/audio/digits/sound_1.mp3',
+        '/problem-solving-correlations-study/audio/sound_effects/error_sound.mp3',
+        '/problem-solving-correlations-study/audio/digits/sound_2.mp3',
+        '/problem-solving-correlations-study/audio/sound_effects/error_sound.mp3',
+        '/problem-solving-correlations-study/audio/digits/sound_3.mp3',
+        '/problem-solving-correlations-study/audio/sound_effects/error_sound.mp3'
+    ];
+
+    let currentSoundIndex = 0
+
+    const playTestSounds = () => {
+        if (currentSoundIndex < soundFiles.length) {
+            const sound = new Howl({
+                src: [soundFiles[currentSoundIndex]],
+                onend: () => {
+                    currentSoundIndex++;
+                    playTestSounds();
+                }
+            });
+
+            sound.play();
+        } else {
+            currentSoundIndex = 0
+        }
+    };
+
+
     const { pages, currentPageIndex, setCurrentPageIndex } = usePageContext();
     return (
         <div className="flex flex-col items-center justify-items-center justify-center min-h-screen">
@@ -24,7 +57,8 @@ export default function Home() {
             <p className="text-lg pb-8">Instructions will be provided before each task begins.</p>
             <p className="text-lg pb-8">Please try to perform as well as possible on each task.</p>
             <p className="text-lg font-bold pb-8">By continuing, you consent to participate in this study, and you agree to the terms described on the participant information sheet</p>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { if (currentPageIndex + 1 < pages.length) {setCurrentPageIndex(currentPageIndex + 1); requestFullscreen()} }}>Start Study</button>
+            <button className="bg-slate-600 hover:bg-slate-700 text-white font-bold py-2 my-5 px-4 rounded-full" onClick={() => currentSoundIndex == 0 ? playTestSounds() : null}>Test Sound</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => { if (currentPageIndex + 1 < pages.length) { setCurrentPageIndex(currentPageIndex + 1); requestFullscreen() } }}>Start Study</button>
         </div>
     );
 }
