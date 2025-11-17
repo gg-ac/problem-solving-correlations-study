58f620f53ef80b1a4ef12a1969cec1a0148516fb
George Gabriel <gandrewgabriel.academic@gmail.com>
2025-11-17 11:00:58 +0000

    Adjusts instructions for memory span and visual search

--- src/components/memory-span/TaskMemorySpanInstructions.tsx
+++ src/components/memory-span/TaskMemorySpanInstructions.tsx
@@ -9,11 +9,16 @@ export default function TaskMemorySpanInstructions() {
                 <span className="text-2xl">Memory Span</span>
             </div>
 
-            <div className=" text-lg row-span-3 grid grid-rows-3 gap-8 px-10 mx-10 rounded-lg border-2 border-dashed">
+            <div className="overflow-auto text-md row-span-3 grid grid-rows-3 gap-8 px-10 mx-10 rounded-lg border-2 border-dashed">
                 <ul>
                     <li className="py-5">You will hear a list of digits read out quickly</li>
                     <li className="py-5">Try to remember as many digits as you can from the <u>end of the list</u> (i.e. the ones you hear last)</li>
                     <li className="py-5">When prompted, enter as many digits as you can remember using the keyboard</li>
+                    <li className="py-5">Example:</li>
+                    <li className="py-2">If you hear the list "9 3 6 5 4 1 3 5 7 2" try to remember the most recent digits</li>
+                    <li className="py-2">If you can only remember the most recent three digits, you'll enter "572"</li>
+                    <li className="py-2">If you can remember the most recent five digits, you'll enter "13572"</li>
+                    <li className="py-2">Try to remember as many digits as you can from the end of the list</li>
                 </ul>
             </div>
 
--- src/components/visual-search/TaskVisualSearchInstructions.tsx
+++ src/components/visual-search/TaskVisualSearchInstructions.tsx
@@ -6,19 +6,46 @@ import StimulusTarget from "./StimulusTarget";
 export default function TaskVisualSearchInstructions() {
 
     return (
-        <div className="h-full w-full grid font-sans grid-rows-5 gap-2 px-60 py-5">
+        <div className="h-full w-full grid font-sans gap-2 px-60 py-5">
 
             <div className="flex items-end py-10">
                 <span className="text-2xl">Visual Search Task</span>
             </div>
 
-            <div className=" text-lg row-span-3 grid grid-rows-3 gap-8 px-10 py-5 mx-10 rounded-lg border-2 border-dashed">
-                <div className="flex items-center justify-center">You will see a square grid containing symbols <span className="h-[32px] mx-3"><StimulusTarget></StimulusTarget></span>  and <span className="h-[32px] mx-3"><StimulusDistractor></StimulusDistractor></span></div>
-                <div className="flex items-center justify-center">Press<KeyboardIcon>J</KeyboardIcon><span> with your right index finger if there <b>is</b> a </span><span className="h-[32px] mx-3"><StimulusTarget></StimulusTarget></span> anywhere in the grid of symbols</div>
-                <div className="flex items-center justify-center">Press<KeyboardIcon>F</KeyboardIcon><span> with your left index finger if there <u>is not</u> a </span><span className="h-[32px] mx-3"><StimulusTarget></StimulusTarget></span>in the grid of symbols</div>
-                <div className="flex items-center justify-center"><span>Try to answer as quickly and accurately as possible</span></div>
-                <div className="flex items-center justify-center"><span>The task will start immediately, so get ready!</span></div>
+            <div className="text-md grid gap-12 px-10 py-5 mx-10 rounded-lg border-2 border-dashed 
+                min-h-[30vh] overflow-y-auto">
+
+                <div className="flex flex-wrap items-center justify-center gap-x-2">
+                    You will see a square grid containing symbols
+                    <span className="flex h-[32px] mx-2"><StimulusTarget /></span>
+                    and
+                    <span className="h-[32px] mx-2"><StimulusDistractor /></span>
+                </div>
+
+                <div className="flex flex-wrap items-center justify-center gap-x-2">
+                    Press <KeyboardIcon>J</KeyboardIcon>
+                    <span>with your right index finger if there <b>is</b> a</span>
+                    <span className="h-[32px] mx-2"><StimulusTarget /></span>
+                    anywhere in the grid of symbols
+                </div>
+
+                <div className="flex flex-wrap items-center justify-center gap-x-2">
+                    Press <KeyboardIcon>F</KeyboardIcon>
+                    <span>with your left index finger if there <u>is not</u> a</span>
+                    <span className="h-[32px] mx-2"><StimulusTarget /></span>
+                    in the grid of symbols
+                </div>
+
+                <div className="flex flex-wrap items-center justify-center">
+                    Try to answer as quickly and accurately as possible
+                </div>
+
+                <div className="flex flex-wrap items-center justify-center">
+                    The task will start immediately, so get ready!
+                </div>
+
             </div>
+
 
             <div className="flex items-end justify-center">
                 <div className="flex items-center text-xl">Press<KeyboardIcon>J</KeyboardIcon>to start</div>
